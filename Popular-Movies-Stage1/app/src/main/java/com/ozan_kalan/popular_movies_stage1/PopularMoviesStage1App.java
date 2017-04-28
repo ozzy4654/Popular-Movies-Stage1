dbfedf46df153314145c7f6440dbf636644f1d1b
Ozan Kalan <ozan.kalan@contextmediainc.com>
2017-04-28 11:40:48 -0500

    Setting up project
    imported libs needed.

---
+++ Popular-Movies-Stage1/app/src/main/java/com/ozan_kalan/popular_movies_stage1/PopularMoviesStage1App.java
@@ -0,0 +1,23 @@
+package com.ozan_kalan.popular_movies_stage1;
+
+import android.app.Application;
+
+import com.squareup.leakcanary.LeakCanary;
+
+/**
+ * Created by ozan.kalan on 4/28/17.
+ */
+
+public class PopularMoviesStage1App extends Application {
+    @Override public void onCreate() {
+
+        super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
+        // Normal app init code...
+    }
+}
